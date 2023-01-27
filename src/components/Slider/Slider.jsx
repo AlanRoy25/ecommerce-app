a0657b970fd13cd12946c96ebb59bc4d2ba67cd7
AlanRoy25 <alanroythomas4@gmail.com>
2023-01-27 14:16:58 +0530

    Added Products images and changed sliders

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -7,8 +7,9 @@ const Slider = () => {
   const [currentSlide, setCurrentSlide] = useState(0);
 
   const data = [
-    "https://images.pexels.com/photos/683929/pexels-photo-683929.jpeg",
-    "https://static.asianpaints.com/content/dam/asian_paints/idea-gallery/sevices-safe-painting-inspiration-hallway-asian-paints.jpg.transform/cc-width-577-height-415/image.jpg",
+    // "https://images.pexels.com/photos/683929/pexels-photo-683929.jpeg",
+    "https://cdn.pixabay.com/photo/2016/07/10/19/18/the-interior-of-the-1508271_960_720.jpg",
+    "https://www.bergerpaints.com/blog/wp-content/uploads/2019/09/Test-on-multiple-walls.png",
     "https://images.akzonobel.com/akzonobel-flourish/dulux/uk/en/dulux-colour-of-the-year-2022/landing-page/Dulux-Colour-Futures-Colour-of-the-Year-2023-COY-LivingRoom-Inspiration-Global-1920x1080%20KV.jpg?impolicy=.auto&imwidth=1366",
   ];
 
